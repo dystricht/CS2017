eebbb04ef02bf4a333200fd80e479e0c6d5b01b1
Dillon Strichman <dstrichman@gmail.com>
2017-02-18 23:13:50 -0500

    first commit

---
+++ InClassWork/README.md
@@ -0,0 +1,4 @@
+# CS2017
+In-class work for CSI213
+
+This is a repository for all in-class work / code examples. It may also include little tidbits of projects I work on in-class.
---
+++ InClassWork/src/com/dystricht/InClassWork/AsciiObject.java
@@ -0,0 +1,21 @@
+package com.dystricht.InClassWork;
+
+public class AsciiObject {
+
+	int x;
+	int y;
+	char sym = 'C';
+	
+	
+	public AsciiObject(){
+		this.x = 0;
+		this.y = 0;
+	}
+	
+	public char getChar(){
+		
+		return this.sym;
+	}
+	
+	
+}
